dc1541223bdb6d138e206ad4a20670045bd6466b
sapphirexdy <98646348+sapphirexdy@users.noreply.github.com>
2026-06-18 07:30:01 +0000

    Deploying to gh-pages from @ sapphirexdy/sapphirexdy.github.io@81d95208d0f3818fbf2eb276c96ebe35a9b2388e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -468,13 +468,6 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/rendercv/rendercv_output/Dunyao_Xue_CV.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -500,7 +493,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=91tAPrQAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=https://scholar.google.com.hk/citations?user=91tAPrQAAAAJ&hl=zh-CN", "_blank");
         },
       },{
       id: 'light-theme',
